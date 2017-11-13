7e8a795bc3adca9aaab037a7c636af469e9c7e2e
vpetryk <vpetryk@softjourn.com>
2017-11-13 15:57:45 +0200

    Dockerfile

--- docker/install.sh
+++ docker/install.sh
@@ -1,3 +1,3 @@
 docker build -t="caapp" .
 
-docker run --network=fixtures_default --mount type=bind,source=/tmp/fabric-client-kvs_peerOrg1,target=/tmp/fabric-client-kvs_peerOrg1 -i -t -p 3005:3000 caapp
+docker run --name=caapp --network=fixtures_default --mount type=bind,source=/tmp/fabric-client-kvs_peerOrg1,target=/tmp/fabric-client-kvs_peerOrg1 -i -t -p 3000:3000 caapp
